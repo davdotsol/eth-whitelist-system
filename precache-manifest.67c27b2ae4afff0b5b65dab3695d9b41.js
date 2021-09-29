d0ff2f6618364b4bcfd104d04d9a6a77a23de3f0
davdotsol <davdotsol@gmail.com>
2021-09-29 21:47:21 +0200

    Updates

---
+++ precache-manifest.67c27b2ae4afff0b5b65dab3695d9b41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d688c63b88301d8e4221fe7e17632e3",
+    "url": "/davdotsol/eth-whitelist-system/index.html"
+  },
+  {
+    "revision": "dba4e4d9a3267a783d7e",
+    "url": "/davdotsol/eth-whitelist-system/static/css/2.9ff99f11.chunk.css"
+  },
+  {
+    "revision": "703e4d76e5f2ff1a7c22",
+    "url": "/davdotsol/eth-whitelist-system/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "dba4e4d9a3267a783d7e",
+    "url": "/davdotsol/eth-whitelist-system/static/js/2.0485bd3f.chunk.js"
+  },
+  {
+    "revision": "703e4d76e5f2ff1a7c22",
+    "url": "/davdotsol/eth-whitelist-system/static/js/main.07763125.chunk.js"
+  },
+  {
+    "revision": "3acad7b67c300af98448",
+    "url": "/davdotsol/eth-whitelist-system/static/js/runtime-main.8b48fbc8.js"
+  }
+]);
